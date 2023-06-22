91af14bc04ae6edee2b6cb6d8d796161600894ee
lequan <lequan2002.ql@gmail.com>
2023-06-22 23:25:43 +0700

    testing project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,171 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [shoesData, setShoesData] = useState(null);
+  const [cardData, setCardData] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0.00);
+
+  useEffect(() => {
+    const storedCartData = localStorage.getItem('cartData');
+    const storedTotalAmount = localStorage.getItem('totalAmount');
+
+    if (storedCartData && storedTotalAmount) {
+      setCardData(JSON.parse(storedCartData));
+      setTotalAmount(parseFloat(storedTotalAmount));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cartData', JSON.stringify(cardData));
+    localStorage.setItem('totalAmount', totalAmount.toFixed(2));
+  }, [cardData, totalAmount]);
+
+  useEffect(() => {
+    fetch('/data/shoes.json')
+      .then(response => response.json())
+      .then(data => {
+        setShoesData(data);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+  }, []);
+  const handleAddToCart = (shoe) => {
+    shoe.amount = 1;
+    setCardData(prevCardData => [...prevCardData, shoe]);
+    setTotalAmount(prevTotalAmount => prevTotalAmount + shoe.price);
+  };
+  const increaseAmount = (shoe) => {
+    const updatedCardData = cardData.map(item => {
+      if (item.id === shoe.id) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    setCardData(updatedCardData);
+    setTotalAmount(prevTotalAmount => prevTotalAmount + shoe.price);
+  };
+
+  const decreaseAmount = (shoe) => {
+    const updatedCardData = cardData.map(item => {
+      if (item.id === shoe.id) {
+        const updatedAmount = item.amount - 1;
+        return { ...item, amount: updatedAmount >= 1 ? updatedAmount : 1 };
+      }
+      return item;
+    });
+
+    if (shoe.amount === 1) {
+      removeProduct(shoe);
+    } else {
+      setCardData(updatedCardData);
+      setTotalAmount(prevTotalAmount => prevTotalAmount - shoe.price);
+    }
+  };
+
+  const removeProduct = (shoe) => {
+    const updatedCardData = cardData.filter(item => item.id !== shoe.id);
+    setCardData(updatedCardData);
+    setTotalAmount(prevTotalAmount => prevTotalAmount - (shoe.price * shoe.amount));
+  };
+  useEffect(() => {
+    return () => {
+      localStorage.removeItem('cartData');
+      localStorage.removeItem('totalAmount');
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wave-animation"></div>
+      <div className="container">
+        <div className="product-section">
+          <div className="card">
+            <div className="card-corner"></div>
+            <div style={{ margin: '12px 0', position: 'relative' }}>
+              <img className="logo-nike" src="/assets/nike.png"></img>
+            </div>
+            <div className="product-header">Our Products</div>
+            {shoesData ? (
+              <div className="shoe-elements">
+                {shoesData.shoes.map(shoe => (
+                  <div className="shoe-item" key={shoe.id}>
+                    <div className="shoe-image-container" style={{ backgroundColor: shoe.color }}>
+                      <img src={shoe.image} alt={shoe.name} />
+                    </div>
+                    <h3 className="shoe-name">{shoe.name}</h3>
+                    <div className="shoe-desc">{shoe.description}</div>
+                    <div className="shoe-bottom">
+                      <div data-testid="shoe-price" className="shoe-price">${shoe.price}</div>
+                      <div >
+                        {cardData.some(item => item.id === shoe.id) ? (
+                          <p data-testid="shoe-add-btn" className="shoe-added">
+                            <img src="/assets/check.png" alt="Done" />
+                          </p>
+                        ) : (
+                          <p data-testid="shoe-add-btn" className="shoe-add-btn" onClick={() => handleAddToCart(shoe)}>
+                            ADD TO CART
+                          </p>
+                        )}
+                      </div>
+                    </div>
+
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>Loading data...</p>
+            )}
+          </div>
+        </div>
+        <div className="cart-section">
+          <div className="card">
+            <div className="card-corner"></div>
+            <div style={{ margin: '12px 0', position: 'relative' }}>
+              <img className="logo-nike" src="/assets/nike.png"></img>
+            </div>
+            <div className="product-header">Your cart
+              <span data-testid="total-price" className="total-price" style={{ float: "right" }}>
+                {totalAmount !== null && totalAmount !== undefined ? (
+                  <div>${totalAmount.toFixed(2)}</div>
+                ) : (
+                  <div>$0.00</div>
+                )}
+              </span>
+            </div>
+            <div className="cart-body">
+              {cardData.length === 0 ? (
+                <div className="cart-empty-noti">Your cart is empty.</div>
+              ) : (
+
+                <div className="products-info">
+                  {cardData.map(shoe => (
+                    <div className="shoe-cart-item" key={shoe.id}>
+                      <div className="shoe-cart-image-container" style={{ backgroundColor: shoe.color }}>
+                        <img src={shoe.image} alt={shoe.name} />
+                      </div>
+                      <div className="shoe-cart-right">
+                        <h3 className="shoe-cart-name">{shoe.name}</h3>
+                        <div className="shoe-cart-price">${shoe.price}</div>
+                        <div class="action">
+                          <div class="amount">
+                            <div class="minus" onClick={() => decreaseAmount(shoe)}>-</div>
+                            <div class="num">{shoe.amount}</div>
+                            <div class="add" onClick={() => increaseAmount(shoe)}>+</div>
+                          </div>
+                          <div data-testid="remove" class="remove" onClick={() => removeProduct(shoe)}>
+                            <img src="/assets/trash.png" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,97 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('add 2 product to card and calculate total price', async () => {
+  //mock data
+  jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+    ok: true,
+    json: async () => ({
+      shoes: [{
+        "id": 1,
+        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
+        "name": "Nike Air Zoom Pegasus 36",
+        "description": "The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.",
+        "price": 108.97,
+        "color": "#e1e7ed"
+      },
+      {
+        "id": 2,
+        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-shield-mens-running-shoe-24FBGb__1_-removebg-preview.png",
+        "name": "Nike Air Zoom Pegasus 36 Shield",
+        "description": "The Nike Air Zoom Pegasus 36 Shield gets updated to conquer wet routes. A water-repellent upper combines with an outsole that helps create grip on wet surfaces, letting you run in confidence despite the weather.",
+        "price": 89.97,
+        "color": "#4D317F"
+      },
+      {
+        "id": 3,
+        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/cruzrone-unisex-shoe-T2rRwS-removebg-preview.png",
+        "name": "Nike CruzrOne",
+        "description": "Designed for steady, easy-paced movement, the Nike CruzrOne keeps you going. Its rocker-shaped sole and plush, lightweight cushioning let you move naturally and comfortably. The padded collar is lined with soft wool, adding luxury to every step, while mesh details let your foot breathe. There’s no finish line—there’s only you, one step after the next.",
+        "price": 100.97,
+        "color": "#E8D026"
+      },
+      ] 
+    }),
+  });
+  //render component
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  await waitFor(async () => {
+    //step execute
+    let addButton = screen.getAllByTestId('shoe-add-btn')[0];
+    fireEvent.click(addButton);
+    addButton = screen.getAllByTestId('shoe-add-btn')[1];
+    fireEvent.click(addButton);
+    //get actual result
+    const totalPriceElement = screen.getByTestId('total-price');
+    //expected result
+    expect(totalPriceElement.textContent).toContain('198.94');
+  });
+});
+test('add 2 product to card remove 1 and calculate total price', async () => {
+  //mock data
+  jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+    ok: true,
+    json: async () => ({
+      shoes: [{
+        "id": 1,
+        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
+        "name": "Nike Air Zoom Pegasus 36",
+        "description": "The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.",
+        "price": 108.97,
+        "color": "#e1e7ed"
+      },
+      {
+        "id": 2,
+        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-shield-mens-running-shoe-24FBGb__1_-removebg-preview.png",
+        "name": "Nike Air Zoom Pegasus 36 Shield",
+        "description": "The Nike Air Zoom Pegasus 36 Shield gets updated to conquer wet routes. A water-repellent upper combines with an outsole that helps create grip on wet surfaces, letting you run in confidence despite the weather.",
+        "price": 89.97,
+        "color": "#4D317F"
+      },
+      {
+        "id": 3,
+        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/cruzrone-unisex-shoe-T2rRwS-removebg-preview.png",
+        "name": "Nike CruzrOne",
+        "description": "Designed for steady, easy-paced movement, the Nike CruzrOne keeps you going. Its rocker-shaped sole and plush, lightweight cushioning let you move naturally and comfortably. The padded collar is lined with soft wool, adding luxury to every step, while mesh details let your foot breathe. There’s no finish line—there’s only you, one step after the next.",
+        "price": 100.97,
+        "color": "#E8D026"
+      },
+      ] 
+    }),
+  });
+  //render component
+  render(<App />);
+  await waitFor(async () => {
+    //step execute
+    let addButton = screen.getAllByTestId('shoe-add-btn')[0];
+    fireEvent.click(addButton);
+    addButton = screen.getAllByTestId('shoe-add-btn')[1];
+    fireEvent.click(addButton);
+    let removeButton = screen.getAllByTestId('remove')[0];
+    fireEvent.click(removeButton);
+    //get actual result
+    const totalPriceElement = screen.getByTestId('total-price');
+    //expected result
+    expect(totalPriceElement.textContent).toContain('89.97');
+  });
 });
